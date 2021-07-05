fcbc365cd2b520f61d4fe79637be74b167c5d5e7
Alex Martin <martin.alex.t@gmail.com>
2021-07-05 19:23:37 -0400

    Clean up some create-react-app cruft.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import reduxThunk from "redux-thunk";
 
 import storeReducer from './reducers.js'
 import App from './App'
-import reportWebVitals from './reportWebVitals';
-
-import './index.css';
 
 const store = createStore(storeReducer, composeWithDevTools(
   applyMiddleware(reduxThunk)
@@ -23,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
