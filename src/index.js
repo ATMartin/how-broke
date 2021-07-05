dce4ccc1f71bcf128cd5bb3dfc1e9b995d965e6d
Alex Martin <martin.alex.t@gmail.com>
2021-07-05 09:00:36 -0400

    Add routing scaffold.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
